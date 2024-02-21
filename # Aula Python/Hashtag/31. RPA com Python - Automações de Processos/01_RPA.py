bc2313b12cfe70372e53197fd103b083990870a8
Cix <gabrielviniciodacosta@gmail.com>
2024-02-21 11:28:08 -0300

    #

--- # Aula Python/Hashtag/31. RPA com Python - Automações de Processos/01_RPA.py
+++ # Aula Python/Hashtag/31. RPA com Python - Automações de Processos/01_RPA.py
@@ -1,11 +1,24 @@
 import pyautogui
+import keyboard
+import mouse
+import time
 
+#time.sleep(1)
 #yautogui.write() -> Escreve
 #pyautogui.click -> Clicka
 #pyautogui.locateOnScreen -> Identifica uma imagem na sua tela
 #pyautogui.hotkey -> usa atalhos do teclado
 #pyautogui.press -> aperta um botão do teclado
-
 #abrir o sistema (abrir o navegador, neste caso Brave)
 #entrar no Gmail
-print(pyautogui.KEYBOARD_KEYS)
+# while True:
+#     print(mouse.get_position())
+#     time.sleep(1)
+pyautogui.PAUSE = 1 #tempo de espera a cada comando
+pyautogui.press('win') #pressionar win
+pyautogui.write('brave') #digitar brave
+pyautogui.press('enter') #pressionar enter
+pyautogui.write('mail google')
+pyautogui.press('enter')
+x, y, largura, altura = pyautogui.locateOnScreen('busca_google.png')
+pyautogui.click(x + largura/2, y + altura/2)

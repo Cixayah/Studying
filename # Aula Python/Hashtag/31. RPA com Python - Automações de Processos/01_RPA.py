3eef0681974c29c6a8d7790515f3de9609d2214c
Cix <gabrielviniciodacosta@gmail.com>
2024-02-21 09:59:08 -0300

    #

--- # Aula Python/Hashtag/31. RPA com Python - Automações de Processos/01_RPA.py
+++ # Aula Python/Hashtag/31. RPA com Python - Automações de Processos/01_RPA.py
@@ -4,4 +4,8 @@ import pyautogui
 #pyautogui.click -> Clicka
 #pyautogui.locateOnScreen -> Identifica uma imagem na sua tela
 #pyautogui.hotkey -> usa atalhos do teclado
-#pyautogui.press -> aperta um botão do teclad
+#pyautogui.press -> aperta um botão do teclado
+
+#abrir o sistema (abrir o navegador, neste caso Brave)
+#entrar no Gmail
+print(pyautogui.KEYBOARD_KEYS)

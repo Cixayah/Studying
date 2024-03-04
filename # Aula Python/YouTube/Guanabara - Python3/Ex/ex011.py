3963ba362130192c526b78a080fad79d51af80d4
Cix <gabrielviniciodacosta@gmail.com>
2024-03-04 11:40:34 -0300

    #

--- # Aula Python/YouTube/Guanabara - Python3/Ex/ex011.py
+++ # Aula Python/YouTube/Guanabara - Python3/Ex/ex011.py
@@ -1,4 +1,3 @@
 #Faça um programa que leia a largura e a altura de uma parede em metros,
 #calcule a sua área e a quantidade de tinta necessária para pinta-la,
 #sabendo que cada litro de tinta pinta área de 2m².
-

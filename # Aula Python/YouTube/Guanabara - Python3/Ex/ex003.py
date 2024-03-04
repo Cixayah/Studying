af8dfd7d327281aee542700a034caa7902955722
Cix <gabrielviniciodacosta@gmail.com>
2024-03-04 10:01:11 -0300

    #

---
+++ # Aula Python/YouTube/Guanabara - Python3/Ex/ex003.py
@@ -0,0 +1,5 @@
+# Crie um programa que leia dois número e mostre a soma entre eles.
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite outro número: '))
+s = n1+n2
+print(f'A soma entre {n1} + {n2} é de {s}')

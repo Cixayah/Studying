12be92fbdfc5d70abf982ce6a0d6a14e6ae29bbd
Cix <gabrielviniciodacosta@gmail.com>
2024-03-04 08:58:58 -0300

    #

---
+++ # Aula Python/YouTube/Guanabara - Python3/Ex/ex002.py
@@ -0,0 +1,6 @@
+#Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas.
+
+msg = input('Por favor, digite seu nome: ')
+print('Seja bem vindo '+msg+'!')
+print(f'Seja bem vindo {msg}!')
+print('Seja bem vindo {}!'.format(msg))

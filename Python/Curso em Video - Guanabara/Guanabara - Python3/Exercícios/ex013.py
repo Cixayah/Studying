cdafd373c40bb369267741ab6188cb9cedc0ba78
Cix <gabrielviniciodacosta@gmail.com>
2024-03-05 16:44:36 -0300

    #

--- Python/Curso em Video - Guanabara/Guanabara - Python3/Exercícios/ex013.py
+++ Python/Curso em Video - Guanabara/Guanabara - Python3/Exercícios/ex013.py
@@ -4,3 +4,8 @@
 n = float(input('Digite seu salário: '))
 ns = n + (n * 0.15)
 print(f'Seu novo salário com 15% de aumento agora é de R${ns:.2f} ')
+
+#correção prof Guanabara
+n = float(input('Digite seu salário: '))
+ns = n + (n * 15/100)
+print(f'Seu novo salário com 15% de aumento agora é de R${ns:.2f} ')

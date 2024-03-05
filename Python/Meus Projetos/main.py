00a663e1b05e23f7dd65e452afd480a6e9f85cb1
Gabriel "Cix" Costa <gabrielviniciodacosta@gmail.com>
2024-03-05 14:16:51 -0300

    #

--- Python/Meus Projetos/main.py
+++ Python/Meus Projetos/main.py
@@ -1,17 +1,27 @@
-from discord.ext import commands, tasks
+import os
 import discord
-from discord import Color
+from discord.ext import commands
 
-intents = discord.Intents.all()  # allowing all intents
-intents.members = True
+intents = discord.Intents.default()
+intents.message_content = True
 
-bot = commands.Bot(command_prefix="!", help_command=None, intents=intents)  # Creating our bot
+bot = commands.Bot(command_prefix='/' , intents=intents)
+my_secret = os.environ['DISCORD_TOKEN']
 
 
-@bot.event
-async def on_ready():
-    try:  # If bot can connect to the discord
+@bot.command(name='teste')
+async def inverse (ctx, message):
+  await ctx.send(message[::-1])
 
-        print('Discord bot succesfully connected')
-    except:
-        print("[!] Couldn't connect, an Error occured")
+@bot.command(name='soma')
+async def soma(ctx, num1: int, num2: int):
+    result = num1 + num2
+    await ctx.send(f'A soma é: {result}')
+  
+@bot.command(name='subtracao')
+async def subtracao(ctx, num1: int, num2: int):
+    result = num1 - num2
+    await ctx.send(f'A subtração é: {result}')
+
+
+bot.run(my_secret)

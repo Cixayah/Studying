e1ab7ae6bda24e11bd476bd5eaa70cb53bbf1449
Cix <gabrielviniciodacosta@gmail.com>
2024-03-05 14:37:54 -0300

    #

--- Python/Curso em Video - Guanabara/Guanabara - Python3/Exercícios/ex009.py
+++ Python/Curso em Video - Guanabara/Guanabara - Python3/Exercícios/ex009.py
@@ -1,5 +1,8 @@
 # Faça um programa que leia um numero inteiro qualquer e mostre na tela a sua tabuada.
 n = int(input('Digite o número para saber a tabuada: '))
+tracinhos = ('🍕' * 10)
+print(tracinhos)
 for c in range(1, 11):
     total = n * c
     print(f'{n} x {c} = {total}')
+print(tracinhos)

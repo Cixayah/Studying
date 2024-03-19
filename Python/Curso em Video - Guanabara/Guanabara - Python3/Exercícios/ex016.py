46ea64f3b30bf1b845d7721f25742cf3bf556872
Cix <gabrielviniciodacosta@gmail.com>
2024-03-19 17:02:07 -0300

    #

--- Python/Curso em Video - Guanabara/Guanabara - Python3/Exercícios/ex016.py
+++ Python/Curso em Video - Guanabara/Guanabara - Python3/Exercícios/ex016.py
@@ -1,2 +1,5 @@
 # Crie um programa que leia um número Real qualquer pelo teclado e mostre na tela a sua porção inteira. EX: Digite um número: 6.127. O número 6.127 tem a parte inteira 6.
 
+from math import trunc
+num = float(input('Digite um valor: '))
+print(f'Você digitou {num} e o valor inteiro é {trunc(num)}')

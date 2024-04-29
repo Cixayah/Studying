3edd9f7f503565b5f10a38ca196d0943c780f2ce
Cix <gabrielviniciodacosta@gmail.com>
2024-04-29 09:02:25 -0300

    #

--- Python/Hashtag/45. Projeto 6 - Criação de Sites com Django - Criando uma cópia do Netflix/Hashflix/movie/models.py
+++ Python/Hashtag/45. Projeto 6 - Criação de Sites com Django - Criando uma cópia do Netflix/Hashflix/movie/models.py
@@ -1,3 +1,22 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+
+
+# Criar filme
+# CATEGORY LIST = ("database", "user")
+CATEGORY_LIST = (
+    ("ANALISES", "Análises"),
+    ("PROGRAMACAO", "Programação"),
+    ("APRESENTACAO","Apresentação"),
+    ("OUTROS", "Outros"),
+)
+class Movie(models.Model):
+    title = models.CharField(max_length=100)
+    thumb = models.ImageField(upload_to='thumbnails')
+    desc = models.TextField(max_length=5000)
+    category = models.CharField(max_length=30, choices=CATEGORY_LIST)
+    views = models.IntegerField(default=0)
+    date_created = models.DateTimeField(default=timezone.now)
+
+# Criar episódio

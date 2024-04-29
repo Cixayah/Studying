642dd8c4561d3c0861b2a36a5394ed810142a59c
Cix <gabrielviniciodacosta@gmail.com>
2024-04-29 10:06:02 -0300

    #

--- Python/Hashtag/45. Projeto 6 - Criação de Sites com Django - Criando uma cópia do Netflix/Hashflix/movie/admin.py
+++ Python/Hashtag/45. Projeto 6 - Criação de Sites com Django - Criando uma cópia do Netflix/Hashflix/movie/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Movie  # ponto significa que o arquivo está na mesma pasta
 
 # Register your models here.
+admin.site.register(Movie)
--- Python/Hashtag/45. Projeto 6 - Criação de Sites com Django - Criando uma cópia do Netflix/Hashflix/movie/models.py
+++ Python/Hashtag/45. Projeto 6 - Criação de Sites com Django - Criando uma cópia do Netflix/Hashflix/movie/models.py
@@ -1,22 +1,40 @@
 from django.db import models
 from django.utils import timezone
+
 # Create your models here.
 
 
 # Criar filme
 # CATEGORY LIST = ("database", "user")
 CATEGORY_LIST = (
-    ("ANALISES", "Análises"),
-    ("PROGRAMACAO", "Programação"),
-    ("APRESENTACAO","Apresentação"),
-    ("OUTROS", "Outros"),
+    ("ACTION", "Ação"),
+    ("ADVENTURE", "Aventura"),
+    ("ANIMATION", "Animação"),
+    ("COMEDY", "Comédia"),
+    ("CRIME", "Crime"),
+    ("DOCUMENTARY", "Documentário"),
+    ("DRAMA", "Drama"),
+    ("FAMILY", "Família"),
+    ("FANTASY", "Fantasia"),
+    ("HORROR", "Terror"),
+    ("MUSICAL", "Musical"),
+    ("ROMANCE", "Romance"),
+    ("SCI-FI", "Ficção Científica"),
+    ("SUSP", "Suspense"),
+    ("THRILLER", "Thriller"),
 )
+
+
+
 class Movie(models.Model):
     title = models.CharField(max_length=100)
-    thumb = models.ImageField(upload_to='thumbnails')
+    thumb = models.ImageField(upload_to="thumbnails")
     desc = models.TextField(max_length=5000)
     category = models.CharField(max_length=30, choices=CATEGORY_LIST)
     views = models.IntegerField(default=0)
     date_created = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.title
 
 # Criar episódio

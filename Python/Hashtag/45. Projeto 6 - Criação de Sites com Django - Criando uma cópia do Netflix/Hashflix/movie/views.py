2014b43ae4feccabade9fdedf37ffc13a34ae9b9
Cix <gabrielviniciodacosta@gmail.com>
2024-05-03 09:32:37 -0300

    #

--- Python/Hashtag/45. Projeto 6 - Criação de Sites com Django - Criando uma cópia do Netflix/Hashflix/movie/views.py
+++ Python/Hashtag/45. Projeto 6 - Criação de Sites com Django - Criando uma cópia do Netflix/Hashflix/movie/views.py
@@ -3,23 +3,25 @@ from .models import Movie
 from django.views.generic import TemplateView, ListView
 
 # Create your views here.
-# def homepage(request):
-#     return render(request, "homepage.html")
 
 
 class Homepage(TemplateView):
     template_name = "homepage.html"
 
 
+class Home_movies(ListView):
+    template_name = "home_movies.html"
+    model = Movie
+    # object_list
+
+
 # url - view - html
+
+# def homepage(request):
+#     return render(request, "homepage.html")
+
 # def home_movies(request):
 #     context = {}  # {} "dicionário" Python
 #     movies_list = Movie.objects.all()
 #     context["movie_list"] = movies_list
 #     return render(request, "home_movies.html", context)
-
-
-class Home_movies(ListView):
-    template_name = "home_movies.html"
-    model = Movie
-    # object_list

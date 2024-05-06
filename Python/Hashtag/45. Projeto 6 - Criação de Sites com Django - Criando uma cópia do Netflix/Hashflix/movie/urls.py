42bd6de23ff8fadcf8a8be306dd823fe832339d1
Cix <gabrielviniciodacosta@gmail.com>
2024-05-06 09:49:30 -0300

    #

--- Python/Hashtag/45. Projeto 6 - Criação de Sites com Django - Criando uma cópia do Netflix/Hashflix/movie/urls.py
+++ Python/Hashtag/45. Projeto 6 - Criação de Sites com Django - Criando uma cópia do Netflix/Hashflix/movie/urls.py
@@ -2,8 +2,12 @@
 from django.urls import path, include
 from .views import Homepage, Home_movies, Movie_details
 
+app_name = "movie"
+
 urlpatterns = [
-    path("", Homepage.as_view()),
-    path("movies/", Home_movies.as_view()),
-    path("movies/<int:pk>", Movie_details.as_view()),  # pk significa primary_key
+    path("", Homepage.as_view(), name="homepage"),
+    path("movies/", Home_movies.as_view(), name="movies"),
+    path(  # pk significa primary_key
+        "movies/<int:pk>", Movie_details.as_view(), name="movie_details"
+    ),
 ]

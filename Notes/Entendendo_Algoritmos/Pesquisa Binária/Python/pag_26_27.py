80efc82e1cb0be52d69648bc11b76e56e9fd35a0
Cix <gabrielviniciodacosta@gmail.com>
2024-05-20 09:23:49 -0300

    ex

--- Notes/Entendendo_Algoritmos/Pesquisa Binária/Python/pag_26_27.py
+++ Notes/Entendendo_Algoritmos/Pesquisa Binária/Python/pag_26_27.py
@@ -13,20 +13,26 @@
 def pesquisa_binaria(lista, item):
     baixo = 0
     alto = len(lista) - 1
+
     # Enquanto ainda não conseguiu chegar a um único elemento...
     while baixo <= alto:
+
         # ...verifica o elemento central.
         meio = (baixo + alto) // 2
         chute = lista[meio]
+
         # Acha o item.
         if chute == item:
             return meio
+
         # O chute foi muito alto.
         if chute > item:
             alto = meio - 1
+
             # O chute foi muito baixo.
         else:
             baixo = meio + 1
+
             # O item não existe.
     return None
 

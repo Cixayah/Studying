124c81c7d4aa4905b88329df31ea69e300e47723
Gabriel "Cix" Costa <gabrielviniciodacosta@gmail.com>
2024-05-27 13:22:51 -0300

    #

--- ADS_Descomplica/ts/script.ts
+++ ADS_Descomplica/ts/script.ts
@@ -1,13 +1,13 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener('DOMContentLoaded', function () {
   // Selecione o botão de entrada
-  const enterBtn: HTMLButtonElement | null = document.querySelector(".primary");
+  const enterBtn: HTMLButtonElement | null = document.querySelector('.primary');
 
   if (enterBtn) {
     // Adicione um evento de clique ao botão de entrada
-    enterBtn.addEventListener("click", function () {
+    enterBtn.addEventListener('click', function () {
       // Selecione o campo de entrada
       const nameInput: HTMLInputElement | null =
-        document.querySelector("#name");
+        document.querySelector('#name');
 
       if (nameInput) {
         // Obtenha o valor inserido no campo de entrada
@@ -15,16 +15,16 @@ document.addEventListener("DOMContentLoaded", function () {
 
         // Selecione o elemento que exibe a mensagem de boas-vindas
         const welcomeMessage: HTMLElement | null =
-          document.querySelector(".subtitle");
+          document.querySelector('.subtitle');
 
         if (welcomeMessage) {
           // Verifique se o campo não está vazio
-          if (name !== "") {
+          if (name !== '') {
             // Se não estiver vazio, exiba a mensagem de boas-vindas com o nome fornecido
             welcomeMessage.textContent = `Bem-Vind@, ${name}!`;
           } else {
             // Se o campo estiver vazio, exiba uma mensagem de erro
-            alert("Por favor, informe seu nome.");
+            alert('Por favor, informe seu nome.');
           }
         }
       }
@@ -33,25 +33,25 @@ document.addEventListener("DOMContentLoaded", function () {
 
   // Selecione o botão de saída
   const exitBtn: HTMLButtonElement | null =
-    document.querySelector(".secondary");
+    document.querySelector('.secondary');
 
   if (exitBtn) {
     // Adicione um evento de clique ao botão de saída
-    exitBtn.addEventListener("click", function () {
+    exitBtn.addEventListener('click', function () {
       // Limpe o valor do campo de entrada
       const nameInput: HTMLInputElement | null =
-        document.querySelector("#name");
+        document.querySelector('#name');
 
       if (nameInput) {
-        nameInput.value = "";
+        nameInput.value = '';
 
         // Selecione o elemento que exibe a mensagem de boas-vindas
         const welcomeMessage: HTMLElement | null =
-          document.querySelector(".subtitle");
+          document.querySelector('.subtitle');
 
         if (welcomeMessage) {
           // Redefina a mensagem de boas-vindas para o valor padrão
-          welcomeMessage.textContent = "Welcome, ";
+          welcomeMessage.textContent = 'Welcome, ';
         }
       }
     });

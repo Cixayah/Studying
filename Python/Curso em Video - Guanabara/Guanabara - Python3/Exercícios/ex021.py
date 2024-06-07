57c3a98ff04d871fa5c9e51a8ec38c7d2bc63033
Gabriel "Cix" Costa <gabrielviniciodacosta@gmail.com>
2024-06-07 09:49:41 -0300

    #

--- Python/Curso em Video - Guanabara/Guanabara - Python3/Exercícios/ex021.py
+++ Python/Curso em Video - Guanabara/Guanabara - Python3/Exercícios/ex021.py
@@ -1,1 +1,17 @@
-# Faça um programa em Python que abra e reproduza o áudio de um arquivo MP3.
+import pygame
+import time
+
+# Inicializa o mixer do pygame
+pygame.mixer.init()
+
+# Carrega o arquivo de música
+pygame.mixer.music.load("./assets/ex021- bedtime - baptiste proud.mp3")
+
+# Reproduz a música
+pygame.mixer.music.play()
+
+# Aguarda até que a música termine
+while pygame.mixer.music.get_busy():
+    time.sleep(1)
+
+print("A reprodução da música terminou.")

a39bcb2cfafd6eddf18b66c95b054a520ce0e9b5
Gabriel "Cix" Costa <gabrielviniciodacosta@gmail.com>
2024-06-07 09:30:35 -0300

    #

--- Python/Curso em Video - Guanabara/Guanabara - Python3/Exercícios/ex020.py
+++ Python/Curso em Video - Guanabara/Guanabara - Python3/Exercícios/ex020.py
@@ -1,18 +1,10 @@
 # O mesmo professor do desafio anterior quer sortear a ordem de apresentação de trabalhos dos alunos. Faça um programa que leia o nome dos quatro alunos e mostre a ordem sorteada.
-from random import choice, shuffle
-from os import system
+from random import shuffle
 
-#Array
-names = ['Larissa', 'Gabriel', 'Lara', 'Alex']
-
-#console clear
-system('clear')
-
-'''for i in range(4):
-    the_chosen = choice(names)
-    names.remove(the_chosen)
-    print(f'A ordem será: {the_chosen}.')'''
-    
-#Correção
-shuffle(names)
-print(f'Esta será a ordem: {names}')
+n1 = str(input("Primeiro aluno: "))
+n2 = str(input("Segundo aluno: "))
+n3 = str(input("Terceiro aluno: "))
+n4 = str(input("Quarto aluno: "))
+list = [n1, n2, n3, n4]
+shuffle(list)
+print(f"A ordem de apresentação será: {list}.")

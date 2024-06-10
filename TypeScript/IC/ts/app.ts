73173e8183ec3f612e408654a3a0099046834695
Gabriel "Cix" Costa <gabrielviniciodacosta@gmail.com>
2024-06-10 09:32:46 -0300

    #

--- TypeScript/IC/ts/app.ts
+++ TypeScript/IC/ts/app.ts
@@ -1,7 +1,1 @@
-function mTable(numberR: number): void {
-  for (let i = 1; i <= 10; i++) {
-    console.log(`${numberR} x ${i} = ${numberR * i}`);
-  }
-}
-const numberR = 2;
-mTable(numberR);
+console.log('Hello World!');

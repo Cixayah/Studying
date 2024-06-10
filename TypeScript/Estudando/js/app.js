bef46f8259f306f86143730fdac2cdbd1f1d04a0
Gabriel "Cix" Costa <gabrielviniciodacosta@gmail.com>
2024-06-10 11:06:20 -0300

    #

---
+++ TypeScript/Estudando/js/app.js
@@ -0,0 +1,15 @@
+"use strict";
+function sayHello(name) {
+    console.log(`Olá, ${name}!`);
+}
+const nome = 'Mundo';
+sayHello(nome);
+let idade = 30;
+let nome2 = 'João';
+let isAtivo = true;
+let numeros = [1, 2, 3, 4, 5];
+let qualquerCoisa = 'qualquer coisa';
+let retorno = undefined;
+let nulo = null;
+let indefinido = undefined;
+let objeto = { chave: 'valor' };
---
+++ TypeScript/Estudando/ts/app.ts
@@ -0,0 +1,16 @@
+function sayHello(name: string) {
+  console.log(`Olá, ${name}!`);
+}
+
+const nome: string = 'Mundo';
+sayHello(nome);
+
+let idade: number = 30;
+let nome2: string = 'João';
+let isAtivo: boolean = true;
+let numeros: number[] = [1, 2, 3, 4, 5];
+let qualquerCoisa: any = 'qualquer coisa';
+let retorno: void = undefined;
+let nulo: null = null;
+let indefinido: undefined = undefined;
+let objeto: object = { chave: 'valor' };

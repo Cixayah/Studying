49e84ae50f3ea27ef273a03020af58867c4c9b5e
Gabriel "Cix" Costa <gabrielviniciodacosta@gmail.com>
2024-06-10 09:21:39 -0300

    #

--- TypeScript/IC/js/app.js
+++ TypeScript/IC/js/app.js
@@ -1,2 +1,6 @@
 "use strict";
-let nome = 'Cix';
+function mTable(numberR) {
+    for (let i = 1; i <= 10; i++) {
+        console.log(`${numberR} x ${i} = ${numberR * i}`);
+    }
+}
--- TypeScript/IC/ts/app.ts
+++ TypeScript/IC/ts/app.ts
@@ -1,1 +1,7 @@
-let nome:string = 'Cix';
+function mTable(numberR: number): void {
+  for (let i = 1; i <= 10; i++) {
+    console.log(`${numberR} x ${i} = ${numberR * i}`);
+  }
+}
+const numberR = 2;
+mTable(numberR);

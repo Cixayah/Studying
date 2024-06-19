acb6cc48f9036f8c8a8e08cd7a91658a4bf39fa8
Gabriel "Cix" Costa <gabrielviniciodacosta@gmail.com>
2024-06-19 17:30:52 -0300

    #

--- TypeScript/Random/js/script.js
+++ TypeScript/Random/js/script.js
@@ -1,10 +1,43 @@
 "use strict";
-function countEvenNumbers(limit) {
-    for (let i = 0; i <= limit; i++) {
-        if (i % 2 === 0) {
-            console.log(i);
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const readline = __importStar(require("readline"));
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+rl.question('Digite o número da tabuada: ', (input) => {
+    let n = parseInt(input, 10);
+    if (isNaN(n)) {
+        console.log('Por favor, digite um número válido');
+    }
+    else {
+        console.log(`tabuada do ${n}`);
+        for (let i = 1; i <= 10; i++) {
+            console.log(`${n} x ${i} = ${n * i}`);
         }
     }
-}
-const limit = 100;
-countEvenNumbers(limit);
+    rl.close();
+});
--- TypeScript/Random/ts/script.ts
+++ TypeScript/Random/ts/script.ts
@@ -1,17 +1,20 @@
-// Definição da função countEvenNumbers
-function countEvenNumbers(limit: number): void {
-  // Loop for que itera de 0 até o limite (inclusive)
-  for (let i = 0; i <= limit; i++) {
-    // Verifica se o número atual (i) é par usando o operador módulo (%)
-    if (i % 2 === 0) {
-      // Se i for par, imprime o valor de i no console
-      console.log(i);
+import * as readline from 'readline';
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question('Digite o número da tabuada: ', (input: string) => {
+  let n: number = parseInt(input, 10);
+  if (isNaN(n)) {
+    console.log('Por favor, digite um número válido');
+  } else {
+    console.log(`tabuada do ${n}`);
+
+    for (let i: number = 1; i <= 10; i++) {
+      console.log(`${n} x ${i} = ${n * i}`);
     }
   }
-}
-
-// Define o limite até onde queremos contar os números pares (neste caso, 100)
-const limit = 100;
-
-// Chama a função countEvenNumbers com o limite especificado
-countEvenNumbers(limit);
+  rl.close();
+});

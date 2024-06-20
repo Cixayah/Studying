07604f7bf6f8f4f4a3906f39b66ddeec43ca2af6
Gabriel "Cix" Costa <gabrielviniciodacosta@gmail.com>
2024-06-20 09:05:33 -0300

    #

--- TypeScript/Random/ts/script.ts
+++ TypeScript/Random/ts/script.ts
@@ -4,17 +4,25 @@ const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
+const askQuestion = () => {
+  rl.question('Digite o número da tabuada: ', (input: string) => {
+    let n: number = parseInt(input, 10);
+    if (isNaN(n)) {
+      console.log('Por favor, digite um número válido');
+    } else {
+      console.log(`tabuada do ${n}`);
 
-rl.question('Digite o número da tabuada: ', (input: string) => {
-  let n: number = parseInt(input, 10);
-  if (isNaN(n)) {
-    console.log('Por favor, digite um número válido');
-  } else {
-    console.log(`tabuada do ${n}`);
-
-    for (let i: number = 1; i <= 10; i++) {
-      console.log(`${n} x ${i} = ${n * i}`);
+      for (let i: number = 1; i <= 10; i++) {
+        console.log(`${n} x ${i} = ${n * i}`);
+      }
+      rl.question('Deseja continuar?: ', (answerQuestion: string) => {
+        if (answerQuestion.toLocaleLowerCase() == 's' || 'sim') {
+          askQuestion();
+        } else {
+          rl.close();
+        }
+      });
     }
-  }
-  rl.close();
-});
+  });
+};
+askQuestion();

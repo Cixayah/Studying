aded951ab022f23cdd5ba332b12b2435d2788ea8
Gabriel "Cix" Costa <gabrielviniciodacosta@gmail.com>
2024-07-26 15:33:16 -0300

    #

--- Python/meus_projetos/Hungar/cogs/lol.py
+++ Python/meus_projetos/Hungar/cogs/lol.py
@@ -2,7 +2,9 @@ import discord
 from discord import app_commands
 from discord.ext import commands
 from dotenv import load_dotenv
-import os, requests
+import os
+
+# import requests
 
 load_dotenv()
 

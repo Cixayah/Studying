b35a4756566f64521db1703f91643adfb466a486
Gabriel "Cix" Costa <gabrielviniciodacosta@gmail.com>
2024-08-27 14:49:59 -0300

    #

--- Udemy/primeiro-app/src/App.js
+++ Udemy/primeiro-app/src/App.js
@@ -2,8 +2,12 @@ function App() {
   return (
     <div>
       <h1>Meu primeiro App</h1>
+      <Name />
     </div>
   );
 }
-
 export default App;
+
+function Name() {
+  return <span>Meu nome é Gabriel</span>;
+}

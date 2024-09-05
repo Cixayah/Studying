3c9647061e5a072129ff80270e416bb987e9f059
Cix <gabrielviniciodacosta@gmail.com>
2024-09-05 09:13:12 -0300

    #

--- ADS_Descomplica/Módulo 1/Desenvolvimento Dinâmico/Elementos do DOM/js/script.js
+++ ADS_Descomplica/Módulo 1/Desenvolvimento Dinâmico/Elementos do DOM/js/script.js
@@ -3,7 +3,22 @@
 //let name = window.prompt("What is your name?").toUpperCase();
 //let lastname = window.prompt("What is your last name?").toUpperCase();
 //alert(name + " " + lastname);
-let n1 = window.prompt("Enter a number");
-let n2 = window.prompt("Enter another number");
-let sum = parseInt(n1) + parseInt(n2);
-alert("The sum of " + n1 + " and " + n2 + " is " + sum);
+// q = window.prompt('Deseja seguir? Sim ou Não').toLowerCase();
+
+// if (q === 'sim') {
+//     let n1 = window.prompt("Enter a number");
+//     let n2 = window.prompt("Enter another number");
+//     let sum = parseInt(n1) + parseInt(n2);
+//     alert("The sum of " + n1 + " and " + n2 + " is " + sum);
+// } else {
+//     alert('😥')
+// }
+
+function confirmDelete() {
+    let userResponse = confirm('Really want to delete this item?');
+    if (userResponse) {
+        alert('Item deleted!');
+    } else {
+        alert('Action cancelled!');
+    }
+}

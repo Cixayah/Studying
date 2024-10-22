721bec11151e36fbad4031c0150ed139f8674f92
Cix <gabrielviniciodacosta@gmail.com>
2024-10-22 09:17:31 -0300

    #

--- Python/meus_projetos/remover_background/main.py
+++ Python/meus_projetos/remover_background/main.py
@@ -1,8 +1,8 @@
 from rembg import remove
 from PIL import Image
 
-input_path = 'entrada.jpg'
-output_path = 'saida.png'
+input_path = './img/entrada.png'
+output_path = './img/saida.png'
 
 input_image = Image.open(input_path)  # Renomeado para evitar conflito
 output_image = remove(input_image)  # Remoção do fundo

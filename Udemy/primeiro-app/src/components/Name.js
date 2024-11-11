9530dcbb5626bbf2ab7099923a0f0fa7f674413b
Cix <gabrielviniciodacosta@gmail.com>
2024-11-11 10:35:20 -0300

    #

--- Udemy/primeiro-app/src/components/Name.js
+++
@@ -1,9 +0,0 @@
-function Name({ aluno, idade }) {
-    return (
-        <div>
-            <span>Olá, {aluno} - idade: {idade} anos. </span>
-            <p>estou aprendendo react!</p>
-        </div>
-    )
-}
-export default Name;

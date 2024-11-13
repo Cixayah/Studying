8a168a84fe6f8713401afefd4d7ee4b628343d19
Cix <gabrielviniciodacosta@gmail.com>
2024-11-13 09:56:39 -0300

    #

--- Python/Curso em Video - Guanabara/Guanabara - Python3/Exercícios/ex022.py
+++ Python/Curso em Video - Guanabara/Guanabara - Python3/Exercícios/ex022.py
@@ -3,10 +3,11 @@
 # - Quantas letras ao todo (sem considerar espaços).
 # - Quantas letras tem o primeiro nome.
 
-name = str(input('Digite seu nome completo: '))
+name = str(input('Digite seu nome completo: ')).strip()
 print('Analisando seu nome...')
 print('Seu nome em maiúsculas é {}'.format(name.upper()))
 print('Seu nome em minúsculas é {}'.format(name.lower()))
 print('Seu nome tem ao todo {} letras'.format(len(name) - name.count(' ')))
-print('Seu primeiro nome tem {} letras'.format(name.find(' ')))
-
+# print('Seu primeiro nome tem {} letras'.format(name.find(' ')))
+cutter = name.split()
+print('Seu primeiro nome é {} e ele tem {} letras'.format(cutter[0], len(cutter[0])))

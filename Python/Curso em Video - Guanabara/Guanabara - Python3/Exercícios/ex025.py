513e1b528365487e0427f20ff40c5ca54457093f
Cix <gabrielviniciodacosta@gmail.com>
2024-11-18 09:09:02 -0300

    #

---
+++ Python/Curso em Video - Guanabara/Guanabara - Python3/Exercícios/ex025.py
@@ -0,0 +1,4 @@
+#Crie um programa que leia o nome de uma pessoa e diga se ela tem "SILVA" no nome.
+
+name = str(input('Digite o nome de uma pessoa: ')).strip()
+print('O nome tem "SILVA"? {}'.format('SILVA' in name.lower()))

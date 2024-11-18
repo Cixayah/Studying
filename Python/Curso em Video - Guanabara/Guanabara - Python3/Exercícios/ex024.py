6937e4c2f48b7b70f03d1f1555af732b339547a0
Cix <gabrielviniciodacosta@gmail.com>
2024-11-18 08:00:42 -0300

    #

--- Python/Curso em Video - Guanabara/Guanabara - Python3/Exercícios/ex024.py
+++ Python/Curso em Video - Guanabara/Guanabara - Python3/Exercícios/ex024.py
@@ -1,9 +1,9 @@
 # Crie um programa que leia o nome de uma cidade e diga se ela começa ou não com o nome "SANTO".
 
 city = str(input('Digite o nome de uma cidade: ')).strip()
-city = city.upper()
+city = city.upper() #caps
 print('A cidade começa com "SANTO"? {}'.format('SANTO' in city.split()[0]))
-# city = city.split()
-# print('A cidade começa com "SANTO"? {}'.format('SANTO' in city[0]))
-# print('A cidade começa com "SANTO"? {}'.format(city[0] == 'SANTO'))
-# print('A cidade começa com "SANTO"? {}'.format(city[0].find('SANTO')))
+
+# Solução do Guanabara
+cid = str(input('Em que cidade você nasceu? ')).strip()
+print(cid[:5].upper() == 'SANTO')

d66b717365e848c471e6b3baf9145ee6627644c7
Cix <gabrielviniciodacosta@gmail.com>
2024-11-19 13:55:08 -0300

    #

--- Python/Curso em Video - Guanabara/Guanabara - Python3/Exercícios/ex026.py
+++ Python/Curso em Video - Guanabara/Guanabara - Python3/Exercícios/ex026.py
@@ -1,8 +1,9 @@
 # Faça um programa que leia uma frase pelo teclado e mostre quantas vezes aparece a letra "A", em que posição ela aparece a primeira vez e em que posição ela aparece a última vez.
 
-name = str(input('Digite uma frase: ')).strip()
-name = name.upper()
-print('A letra "A" aparece {} vezes.'.format(name.count('A')))
-print('A primeira letra "A" aparece na posição {}.'.format(name.find('A')+1))
-print('A última letra "A" aparece na posição {}.'.format(name.rfind('A')+1))
+phrase = str(input('Digite uma frase: ')).strip()
+phrase = phrase.upper()
+print(f'A frase foi: {phrase}')
+print('A letra "A" aparece {} vezes.'.format(phrase.count('A')))
+print('A primeira letra "A" aparece na posição {}.'.format(phrase.find('A')+1))
+print('A última letra "A" aparece na posição {}.'.format(phrase.rfind('A')+1))
 print('Fim do programa.')

1189e7b86e1888601c3f8be4d4e137dedd4d3fcd
Cix <gabrielviniciodacosta@gmail.com>
2024-11-22 10:51:22 -0300

    #

--- Python/Curso em Video - Guanabara/Guanabara - Python3/Exercícios/ex027.py
+++ Python/Curso em Video - Guanabara/Guanabara - Python3/Exercícios/ex027.py
@@ -4,5 +4,6 @@
 # último = Souza
 
 nome = str(input('Digite seu nome completo: ')).strip().split()
+print(nome)
 print('Seu primeiro nome é: {}'.format(nome[0]))
 print('Seu último nome é: {}'.format(nome[len(nome)-1]))

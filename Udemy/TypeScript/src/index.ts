fe56dc76d6129c5c7aa50557eb366423cf34f0d1
Cix <gabrielviniciodacosta@gmail.com>
2025-03-17 15:14:07 -0300

    #

--- Udemy/TypeScript/src/index.ts
+++ Udemy/TypeScript/src/index.ts
@@ -1,4 +1,4 @@
 const name_user = 'Cix'
 const age = 27
 
-console.log(`Hello, my name is ${name} and I am ${age} years old.`)
+console.log(`Hello, my name is ${name_user} and I am ${age} years old.`)

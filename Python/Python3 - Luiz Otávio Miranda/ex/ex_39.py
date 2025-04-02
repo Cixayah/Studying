4e1a662f1cb4d1259fb6f0771f9f2b8b0917f42d
Cix <gabrielviniciodacosta@gmail.com>
2025-04-02 09:50:50 -0300

    #

--- Python/Python3 - Luiz Otávio Miranda/ex/ex_39.py
+++ Python/Python3 - Luiz Otávio Miranda/ex/ex_39.py
@@ -1,7 +1,7 @@
 login = input('[E]ntrar [S]air: ')
 psw = input('Senha: ')
 psw_c = '123'
-if login == 'E' and psw == psw_c:
+if (login == 'E' or 'e') and psw == psw_c:
     print('Login efetuado')
 else:
     print('Sair')

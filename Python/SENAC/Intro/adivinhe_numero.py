cb929b5c1966df0cc989f1b4b4966f21749dc4d2
Cix <gabrielviniciodacosta@gmail.com>
2025-04-12 10:54:49 -0300

    #

--- Python/SENAC/Intro/adivinhe_numero.py
+++ Python/SENAC/Intro/adivinhe_numero.py
@@ -9,5 +9,13 @@ secret_number = random.randrange(0,100)
 
 max_attempts = 10
 
-for round in range(1,max_attempts +1):
-    print(f'\nTentativa {round:02d} de {max_attempts}')
+for round in range(1, max_attempts + 1):
+    print(f'Tentativa {round:02d} de {max_attempts:02d}')
+    
+    attempt = input('Tente acertar o número de 1 a 100: ')
+    print('Você digitou', attempt)
+
+    attempts_int = int(attempt)
+    if attempts_int < 1 or attempts_int > 100:
+        print('Tentativa inválida. Somente números de 1 a 100.')
+        continue

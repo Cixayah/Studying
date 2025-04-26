ab40566b50b892f1a4869e5a8363281e6d37321c
Cix <gabrielviniciodacosta@gmail.com>
2025-04-26 10:44:33 -0300

    #

---
+++ Python/SENAC/Intro/03_forca.py
@@ -0,0 +1,5 @@
+import forca.desenho as desenhos
+def gg():
+    pass
+if(__name__ == "__main"):
+    gg()
---
+++ Python/SENAC/Intro/forca/func.py
@@ -0,0 +1,31 @@
+import random
+from os import path
+def print_opening_msg():
+    print('*****************************')
+    print('------- JOGO DA FORCA -------')
+    print('*****************************')
+    
+def set_theme():
+    tip = int(input("""
+                    Digite uma opção para o tema:
+                    1 - Carros
+                    2 - Cidades do Brasil 
+                    3 - Países
+                    4 - Nome de pessoas
+                    5 - Frutas """))
+    
+    tips = ('carros' ,'cidade' ,'país' ,'nome' ,'fruta')
+    return tips[tip-1]
+def load_secret_words(theme):
+    dir_name = path.dirname(path.abspath(__file__))
+    archive = open(f'{dir_name}\\{theme}.txt','r')
+    words = []
+    for line in archive:
+        line = line.strip()
+        words.append(line)
+    archive.close
+    number = random.randrange(0, len(words)+1)
+    secret_word = words[number].upper()
+    return secret_word
+def init_correct_letter(secret_word):
+    return ['_' for letter in secret_word]

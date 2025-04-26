1e33870e671cda47cd57f5ebdacde9c4aa1b867a
Cix <gabrielviniciodacosta@gmail.com>
2025-04-26 09:28:20 -0300

    #

--- Python/SENAC/Intro/02_PedraPapelTesoura.py
+++ Python/SENAC/Intro/02_PedraPapelTesoura.py
@@ -1,24 +1,26 @@
 from os import system,name
 import random
-
-option = 's'
-while option.upper() == 'S':
-    system('cls')  if(name == 'nt') else system('clear')
-    
-    cpu = random.randint(0,2)
-    player = int(input('''Escolha uma opção para jogar: 
-                       [1] Pedra
-                       [2] Papel
-                       [3] Tesoura
-                       Digite sua escolha: '''))-1
-    opt_choices = ('Pedra','Papel', 'Tesoura')
-    table = (
-        ('NINGUÉM','JOGADOR','CPU'),
-        ('CPU','NINGUÉM','JOGADOR'),
-        ('JOGADOR','CPU','NINGUÉM')
-    )
-    
-    print(f'Você escolheu{opt_choices[player]}')
-    print(f'A CPU escolheu{opt_choices[cpu]}')
-    print(f'{table[cpu][player]} GANHOU!!!')
-    opt_choices=input('Jogar novamente? Aperte (S)')
+def gg():
+    option = 's'
+    while option.upper() == 'S':
+        system('cls')  if(name == 'nt') else system('clear')
+        
+        cpu = random.randint(0,2)
+        player = int(input('''Escolha uma opção para jogar: 
+                        [1] Pedra
+                        [2] Papel
+                        [3] Tesoura
+                        Digite sua escolha: '''))-1
+        opt_choices = ('Pedra','Papel', 'Tesoura')
+        table = (
+            ('NINGUÉM','JOGADOR','CPU'),
+            ('CPU','NINGUÉM','JOGADOR'),
+            ('JOGADOR','CPU','NINGUÉM')
+        )
+        
+        print(f'Você escolheu{opt_choices[player]}')
+        print(f'A CPU escolheu{opt_choices[cpu]}')
+        print(f'{table[cpu][player]} GANHOU!!!')
+        opt_choices=input('Jogar novamente? Aperte (S)')
+if(__name__ == "__main"):
+    gg()

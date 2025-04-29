ec89ca886906b71514f5b7ec4e715e37d62ffb1e
Cix <gabrielviniciodacosta@gmail.com>
2025-04-29 09:01:43 -0300

    #

--- Python/Curso em Video - Guanabara/Guanabara - Python3/pythonteste/aula10a.py
+++ Python/Curso em Video - Guanabara/Guanabara - Python3/pythonteste/aula10a.py
@@ -1,5 +1,16 @@
+#if else
 name = str(input('Qual seu nome? ').strip().lower())
 if name == 'cix':
     print(f'Olá administrador {name.capitalize()}.')
 else:
     print(f'Olá usuário {name.capitalize()}.')
+    
+n1 = float(input('Digite a primeira nota: '))
+n2 = float(input('Digite a segunda nota: '))
+m = (n1+n2)/2 
+print(f'A sua média foi {m:.1f}')
+if m >= 6:
+    print('Aprovado(a)!')
+else:
+    print('Reprovado(a)!')
+print('PARABÉNS' if m >=6 else 'ESTUDE MAIS!')

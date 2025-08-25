589d3de309c24aafb90875e1064e795c8211c61e
Cix <gabrielviniciodacosta@gmail.com>
2025-08-25 09:44:07 -0300

    feat: add initial HTML, CSS, and JavaScript files for button color change functionality

---
+++ Udemy/LOM - JS Udemy/JS_45/index.js
@@ -0,0 +1,6 @@
+//condicao ? valorSeVerdadeiro : valorSeFalso;
+
+const userPoints = 50;
+const message = userPoints >= 500 ? "Usuário VIP" : "Usuário comum";
+
+console.log(message); //nível de usuário

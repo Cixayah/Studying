6529a612616f9259293a992dfce310c06583634d
Cix <gabrielviniciodacosta@gmail.com>
2025-09-15 19:39:41 -0300

    feat: add comparison operators documentation and example to index.js

---
+++ Udemy/LOM - JS Udemy/JS_37/index.js
@@ -0,0 +1,15 @@
+/* Operadores de comparação
+== igual
+=== estritamente igual
+!= diferente
+!== estritamente diferente
+> maior que
+< menor que
+>= maior ou igual a
+<= menor ou igual a
+? operador ternário
+&& e
+|| ou
+! negação
+*/
+console.log(10 == '10') // true

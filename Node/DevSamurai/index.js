adc88493b7004a1f72fa6b5cb79b7617c5c8ea43
Cix <gabrielviniciodacosta@gmail.com>
2025-10-10 10:11:23 -0300

    feat: update /hello route to handle query parameters and improve response message

--- Node/DevSamurai/index.js
+++ Node/DevSamurai/index.js
@@ -4,17 +4,30 @@ const server = express();
 //http://localhost:3000/hello?name=Cix&age=28
 // Query parameters = ?name=Cix&age=28
 
+server.get('/hello', (req, res) => {
+  const { name, age } = req.query;
+  //para criar uma constante com mais de um valor, usar desestruturação
+  // const { name, age } = req.query;
+  // console.log(name, age);
+
+  res.json({
+    title: 'Welcome to JSON with Node.js!',
+    message: `Hello ${name}, you have ${age} years, welcome to our API!`,
+  });
+});
 // http://localhost:3000/hello/Cix
 // Route parameters = /users/:name
 // Body parameters = { "name": "Cix", "age": 28 }
 
+server.get('/hello/:name', (req, res) => {
+  const name = req.params.name;
 
-server.get('/hello', (req, res) => {
   res.json({
-    title: 'Hello World!',
-    message: 'This is a simple Express server response.'
+    title: 'Welcome to JSON with Node.js!',
+    message: `Hello ${name}, welcome to our API!`,
   });
 });
+
 
 server.listen(3000, () => {
   console.log(`Server is running on port http://localhost:3000`);

ba7b074d54aa3920e273a46b12536aebcf4bbcd4
Cix <gabrielviniciodacosta@gmail.com>
2025-10-13 14:16:19 -0300

    feat: rewrite index.ts to include multiple utility functions with console logs

--- YouTube/TS2025_DevOdair/src/index.ts
+++ YouTube/TS2025_DevOdair/src/index.ts
@@ -1,4 +1,29 @@
-const sayMyName = (name: string): string => {
-    return `My name is ${name}`;
-};
-console.log(sayMyName("John Doe"));
+// 1 - Crie uma função que retorne "Hello, World!" no console
+function helloWorld() {
+    return 'Hello, World!';
+}
+console.log(helloWorld()); //Hello, World!
+
+// 2 - A função recebe um parâmetro do tipo number e retorna o resultado da multiplicação por 2
+function returnByTwo(num: number) {
+    return num * 2
+}
+console.log(returnByTwo(5)); //10
+
+// 3 - Crie uma função que recebe duas palavras (strings) e retorna sua concatenação. De forma resumida, deve juntar as duas palavras.
+function returnTwoWords(w1: string, w2: string) {
+    return w1 + ' ' + w2;
+}
+console.log(returnTwoWords('Vai', 'Corinthians')); //Vai Corinthians
+
+// 4 -  Crie uma função que recebe um número e retorna true se for positivo.
+function numBoolean(isTrue: number): boolean {
+    return isTrue > 0
+}
+console.log(numBoolean(-1)); //false
+
+// 5 - Crie uma função que recebe um número e retorna seu antecessor. Por ex, se o número for 5, retorne 4.
+function numberPrev(numPrev: number) {
+    return numPrev -1
+}
+console.log(numberPrev(10)); //9
